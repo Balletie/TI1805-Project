1eb4840ae5c24d90491475bec3d95d08628e98d3
Balletie <skip_meesie@hotmail.com>
2014-06-08 13:07:47 +0200

    Add new performRaytracing to header, add Shapes classes without implementation

---
+++ src/shapes.cpp
@@ -0,0 +1,17 @@
+#include "shapes.h"
+
+Shape::Shape(Vec3Df color, Vec3Df org) : _color(color), _origin(org)
+{}
+
+Sphere::Sphere(Vec3Df color, Vec3Df org, uint8_t rad) : Shape(color, org), _radius(rad)
+{}
+
+Plane::Plane(Vec3Df color, Vec3Df org, Vec3Df coeff) : Shape(color, org), _coeff(coeff)
+{}
+
+bool Sphere::intersect(const Vec3Df& origin, const Vec3Df& dir, Vec3Df& new_origin, Vec3Df& normal) {
+}
+
+bool Plane::intersect(const Vec3Df& origin, const Vec3Df& dir, Vec3Df& new_origin, Vec3Df& normal) {
+}
+
---
+++ src/shapes.h
@@ -0,0 +1,27 @@
+#ifndef __SHAPE_H__
+#define __SHAPE_H__
+#include "Vec3D.h"
+
+class Shape {
+  public:
+	Shape(Vec3Df, Vec3Df);
+	virtual bool intersect(const Vec3Df&, const Vec3Df&, Vec3Df&, Vec3Df&) = 0;
+  	const Vec3Df _color;
+	const Vec3Df _origin;
+};
+
+class Sphere : public Shape {
+  public:
+	Sphere(Vec3Df, Vec3Df, uint8_t);
+	bool intersect(const Vec3Df&, const Vec3Df&, Vec3Df&, Vec3Df&);
+	const uint8_t _radius;
+};
+
+class Plane : public Shape {
+  public:
+	Plane(Vec3Df, Vec3Df, Vec3Df);
+	bool intersect(const Vec3Df&, const Vec3Df&, Vec3Df&, Vec3Df&);
+	const Vec3Df _coeff;
+};
+
+#endif
