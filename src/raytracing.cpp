f5f66e55746c09a5d1a054323a8cc30a99baeaa1
Balletie <skip_meesie@hotmail.com>
2014-06-18 09:22:11 +0200

    Merge conflict

--- src/raytracing.cpp
+++ src/raytracing.cpp
@@ -3,12 +3,17 @@
 #include <windows.h>
 #endif
 #include <GL/glut.h>
+#include <float.h>
+#include <stdint.h>
 #include "raytracing.h"
-
+#include "shapes.h"
 
 //temporary variables
 Vec3Df testRayOrigin;
 Vec3Df testRayDestination;
+Vec3Df new_orig;
+Vec3Df new_dest;
+std::vector<Shape*> shapes;
 
 //use this function for any preprocessing of the mesh.
 void init()
@@ -17,39 +22,96 @@ void init()
 	//feel free to replace cube by a path to another model
 	//please realize that not all OBJ files will successfully load.
 	//Nonetheless, if they come from Blender, they should.
-    //MyMesh.loadMesh("/lhome/martin/Projects/Uebung/raytracing/cube.obj", true);
-    MyMesh.loadMesh("cube.obj", true);
-    //MyMesh.loadMesh("cube2.obj", true);
-    //MyMesh.loadMesh("dodgeColorTest.obj", true);
+	//MyMesh.loadMesh("/lhome/martin/Projects/Uebung/raytracing/cube.obj", true);
+	//MyMesh.loadMesh("cube.obj", true);
+	//MyMesh.loadMesh("cube2.obj", true);
+	//MyMesh.loadMesh("dodgeColorTest.obj", true);
+	MyMesh.loadMesh("Pen_subsurf.obj", true);
 	MyMesh.computeVertexNormals();
 
 	//one first move: initialize the first light source
 	//at least ONE light source has to be in the scene!!!
 	//here, we set it to the current location of the camera
-	MyLightPositions.push_back(MyCameraPosition);
+	MyLightPositions.push_back(MyCameraPosition + Vec3Df(0, 4, 0));
+
+	/*
+	shapes.push_back(new Sphere(Vec3Df(0.2, 0  , 0  ), Vec3Df(0.2, 0.2, 0.2), Vec3Df(-2, 0, -1), 1));
+	shapes.push_back(new Sphere(Vec3Df(0  , 0  , 0.2), Vec3Df(0.2, 0.2, 0.2), Vec3Df( 0, 0, -1), 1));
+	shapes.push_back(new Sphere(Vec3Df(0.4, 0.4, 0  ), Vec3Df(0.2, 0.2, 0.2), Vec3Df( 0, 2, -1), 1));
+	shapes.push_back(new Sphere(Vec3Df(0.1, 0.1, 0.1), Vec3Df(1  , 1  , 1  ), Vec3Df( 2, 0, -1), 1));
+	*/
+	// Plane(color, origin, coeff)
+	// Horizontal green plane
+	shapes.push_back(new Plane(Vec3Df(0.2,0.2,0.2), Vec3Df(0.5,0.5,0.5), Vec3Df(0,-1,0), Vec3Df(0,1,0)));
+	// Vertical red plane
+	//shapes.push_back(new Plane(Vec3Df(0.2,0,0), Vec3Df(0,0,0), Vec3Df(0,0,1)));
+
 }
 
 //return the color of your pixel.
 Vec3Df performRayTracing(const Vec3Df & origin, const Vec3Df & dest)
 {
-	// Test intersection with circle at origin
-	// Coefficients for the polynomial
-	float a = Vec3Df::dotProduct(dest, dest);
-	float b = 2 * Vec3Df::dotProduct(dest, origin);
-	float c = Vec3Df::dotProduct(origin, origin) - 1.5 * 1.5;
-
-	// Compute discriminant
-	float disc = b * b - 4 * a * c;
-	if (disc < 0) 	return Vec3Df(0,0,0);
-	if (disc == 0)	return Vec3Df(0,0,1);
-	if (disc > 0)	return Vec3Df(1,0,0);
-	//float t0 = (-b - sqrtf(disc)) / 2 * a;
-	//float t1 = (-b + sqrtf(disc)) / 2 * a;
-
-	// Check which of the two is the right scalar to use.
-	//return t0 < t1 ? camPos + t0 * ray : camPos + t1 * ray;
+	// Get the direction of the vector.
+	Vec3Df dir = dest - origin;
+	dir.normalize();
+	return performRayTracing(origin, dir, 0, 6);
 }
 
+Vec3Df performRayTracing(const Vec3Df & origin, const Vec3Df & dir, uint8_t level, uint8_t max)
+{
+	// This will be instantiated with the new normal at the intersection point.
+	Vec3Df normal;
+	// This will be instantiated with the coordinates of the intersection point.
+	Vec3Df new_origin;
+
+	// The color if the intersected object.
+	Vec3Df color;
+	// The specularity of the intersected object.
+	Vec3Df specular;
+
+	float current_depth = FLT_MAX;
+	bool intersection = false;
+
+	for (unsigned int i = 0; i < shapes.size(); i++) {
+		Vec3Df new_new_origin;
+		Vec3Df new_normal;
+		if (shapes[i]->intersect(origin, dir, new_new_origin, new_normal)) {
+			intersection = true;
+			float new_depth = (new_new_origin - origin).getLength();
+			if (new_depth < current_depth) {
+				current_depth = new_depth;
+				normal = new_normal;
+				new_origin = new_new_origin;
+				color = shapes[i]->_color;
+				specular = shapes[i]->_specular;
+			}
+		}
+	}
+	if (!intersection)	return Vec3Df(0.f,0.f,0.f);
+
+	// Re-use intersection here
+	intersection = false;
+	for (unsigned int i = 0; i < shapes.size(); i++) {
+		Vec3Df stub1;
+		Vec3Df stub2;
+		Vec3Df lightPos = MyLightPositions[0] - new_origin;
+		float lightDist = lightPos.getLength();
+
+		if (shapes[i]->intersect(new_origin, lightPos, stub1, stub2)) {
+			if ((stub1 - new_origin).getLength() < lightDist)
+				intersection = true;
+		}
+	}
+	// If there was an intersection, this spot is occluded.
+	if (intersection)	return Vec3Df(0.f,0.f,0.f);
+
+	normal.normalize();
+	// Compute the reflection vector for the next recursive call.
+	Vec3Df reflect = dir - 2 * Vec3Df::dotProduct(dir, normal) * normal;
+
+	if (++level == max)	return color;
+	else			return color + specular * performRayTracing(new_origin, reflect, level, max);
+}
 
 void yourDebugDraw()
 {
@@ -59,15 +121,25 @@ void yourDebugDraw()
 	//as an example: 
 	glPushAttrib(GL_ALL_ATTRIB_BITS);
 	glDisable(GL_LIGHTING);
+
+	// Draw all the shapes for the viewport window.
+	for (int i = 0; i < shapes.size(); i++) {
+		shapes[i]->draw();
+	}
+
 	glColor3f(0,1,1);
 	glBegin(GL_LINES);
-	glVertex3f(testRayOrigin[0], testRayOrigin[1], testRayOrigin[2]);
-	glVertex3f(testRayDestination[0], testRayDestination[1], testRayDestination[2]);
+		glVertex3f(testRayOrigin[0], testRayOrigin[1], testRayOrigin[2]);
+		glVertex3f(testRayDestination[0], testRayDestination[1], testRayDestination[2]);
+		glVertex3f(new_orig[0], new_orig[1], new_orig[2]);
+		glVertex3f(new_dest[0], new_dest[1], new_dest[2]);
 	glEnd();
+
 	glPointSize(10);
 	glBegin(GL_POINTS);
-	glVertex3fv(MyLightPositions[0].pointer());
+		glVertex3fv(MyLightPositions[0].pointer());
 	glEnd();
+
 	glPopAttrib();
 
 }
@@ -80,5 +152,32 @@ void yourKeyboardFunc(char t, int x, int y)
 	//here I use it to get the coordinates of a ray, which I then draw in the debug function.
 	produceRay(x, y, testRayOrigin, testRayDestination);
 
-	std::cout<<t<<" pressed! The mouse was in location "<<x<<","<<y<<"!"<<std::endl;
+	Vec3Df orig = testRayOrigin;
+	Vec3Df dir = testRayDestination - testRayOrigin;
+	dir.normalize();
+
+	Vec3Df normal;
+	if (shapes[0]->intersect(orig, dir, new_orig, normal)) {
+		printf("intersection!!\n");
+		Vec3Df reflect = dir - 2 * Vec3Df::dotProduct(dir, normal) * normal;
+		new_dest = 20 * reflect;
+	}
+
+
+	std::cout<< t <<" pressed! The mouse was in location "<<x<<","<<y<<"!"<<std::endl;
+
+	/*
+	Triangle triangle;
+	Vertex vertex;
+	for (int i = 0; i < MyMesh.triangles.size(); i++) {
+		triangle = MyMesh.triangles[i];
+		printf("triangle index v: %d\t %d\t %d\n", triangle.v[0], triangle.v[1], triangle.v[2]);
+		printf("triangle index t: %d\t %d\t %d\n", triangle.t[0], triangle.t[1], triangle.t[2]);
+		for (int j = 0; j < 3; j++) {
+			vertex = MyMesh.vertices[triangle.v[j]];
+			printf("triangle coords: %f\t %f\t %f\n", vertex.p[0], vertex.p[1], vertex.p[2]);
+		}
+	}
+	*/
 }
+>>>>>>> Stashed changes
---
+++ src/shapes.cpp
@@ -0,0 +1,83 @@
+#include "shapes.h"
+#include <GL/glut.h>
+#include <stdio.h>
+
+static const float EPSILON = 1e-4;
+
+Shape::Shape(Vec3Df color, Vec3Df specular, Vec3Df org)
+: _color(color), _specular(specular), _origin(org)
+{}
+
+Sphere::Sphere(Vec3Df color, Vec3Df specular, Vec3Df org, float rad)
+: Shape(color, specular, org), _radius(rad)
+{}
+
+Plane::Plane(Vec3Df color, Vec3Df specular, Vec3Df org, Vec3Df coeff)
+: Shape(color, specular, org), _coeff(coeff)
+{}
+
+bool Sphere::intersect(const Vec3Df& origin, const Vec3Df& dir, Vec3Df& new_origin, Vec3Df& normal) {
+	Vec3Df trans_origin = origin - this->_origin;
+	float a = Vec3Df::dotProduct(dir, dir);
+	float b = 2 * Vec3Df::dotProduct(trans_origin, dir);
+	float c = Vec3Df::dotProduct(trans_origin, trans_origin) - this->_radius * this->_radius;
+
+	float disc = b * b - 4 * a * c;
+	if (disc < 0)	return false;
+
+	// We use the following in place of the quadratic formula for
+	// more numeric precision.
+	float q = (b > 0) ?
+				-0.5 * (b + sqrtf(disc)) :
+				-0.5 * (b - sqrtf(disc));
+	float t0 = q / a;
+	float t1 = c / q;
+	if (t0 < t1) std::swap(t0,t1);
+
+	float t;
+	if (t0 < EPSILON)	return false;
+	if (t1 < 0)		t = t0;
+	else			t = t1;
+
+	normal = trans_origin + t * dir;
+	normal.normalize();
+	new_origin = origin + t * dir;
+	return true;
+}
+
+void Sphere::draw() {
+	glPushMatrix();
+
+	glTranslatef(this->_origin[0],this->_origin[1],this->_origin[2]);
+	glColor3f(this->_color[0],this->_color[1],this->_color[2]);
+	glutSolidSphere(this->_radius, 20, 20);
+
+	glPopMatrix();
+}
+
+bool Plane::intersect(const Vec3Df& origin, const Vec3Df& dir, Vec3Df& new_origin, Vec3Df& normal)
+{
+	normal = _coeff;
+	normal.normalize();
+
+	float denom = Vec3Df::dotProduct(dir,normal);
+	if (denom < 1e-4 && denom > -1e-4) return false;
+
+	// Calculate term t in the expressen 'p = o + tD'
+	float t = Vec3Df::dotProduct(_origin - origin, normal) / denom;
+	if (t < EPSILON) return false;
+
+	new_origin = origin + t * dir;
+	return true;
+}
+
+void Plane::draw() {
+	glPushMatrix();
+
+	glTranslatef(this->_origin[0],this->_origin[1],this->_origin[2]);
+	glColor3f(this->_color[0],this->_color[1],this->_color[2]);
+	glScalef(10,0.4,10);
+	glutSolidCube(1);
+
+	glPopMatrix();
+}
