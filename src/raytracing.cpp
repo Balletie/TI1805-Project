cf5183c14e4bad09ccbc9f3d60e15e645c628534
Balletie <skip_meesie@hotmail.com>
2014-06-19 14:59:21 +0200

    Change load function to load the low poly version

--- src/raytracing.cpp
+++ src/raytracing.cpp
@@ -23,7 +23,7 @@ void init()
 	//please realize that not all OBJ files will successfully load.
 	//Nonetheless, if they come from Blender, they should.
 	//MyMesh.loadMesh("cube.obj", true);
-	MyMesh.loadMesh("Pen_subsurf.obj", true);
+	MyMesh.loadMesh("Pen_low.obj", true);
 	MyMesh.computeVertexNormals();
 
 	//one first move: initialize the first light source
