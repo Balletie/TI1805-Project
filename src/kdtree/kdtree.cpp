102b92e31641f395807fe09a155ccb17ba962ad8
Balletie <skip_meesie@hotmail.com>
2014-06-22 16:44:23 +0200

    Add the actual files for KDTree

---
+++ src/kdtree/kdtree.cpp
@@ -0,0 +1,89 @@
+#include "kdnode.h"
+#include "kdtree.h"
+
+KDTree::KDTree(std::vector<OurTriangle*>& triangles) {
+	_root = build(triangles);
+}
+
+bool KDTree::intersect(const Vec3Df& origin, const Vec3Df& dir, Vec3Df& new_origin, Vec3Df& normal) {
+	OurTriangle* pt;
+	float depth = FLT_MAX;
+	bool success = _root->intersect(_root, origin, dir, pt, new_origin, normal, depth);
+	_intersected = pt;
+	return success;
+}
+
+Vec3Df KDTree::shade(const Vec3Df& cam_pos, const Vec3Df& intersect, const Vec3Df& light_pos, const Vec3Df& normal) {
+	if (this->hasMat()) {
+		return _intersected->shade(cam_pos, intersect, light_pos, normal);
+	} else {
+		return Vec3Df(0.f,0.f,0.f);
+	}
+}
+
+KDNode* KDTree::build(std::vector<OurTriangle*>& tris) {
+	KDNode* node = new KDNode();
+	node->triangles = tris;
+	node->left = 0;
+	node->right = 0;
+	node->bbox = BoundingBox();
+
+	if (tris.size() == 0) return node;
+
+	node->bbox = tris[0]->getBoundingBox();
+	node->left = new KDNode();
+	node->right = new KDNode();
+	node->left->triangles = std::vector<OurTriangle*>();
+	node->right->triangles = std::vector<OurTriangle*>();
+
+	if (tris.size() == 1) return node;
+
+	for (int i = 1; i < tris.size(); i++) {
+		node->bbox.expand(tris[i]->getBoundingBox());
+	}
+
+	Vec3Df midpt = Vec3Df(0,0,0);
+	for (int i = 0; i < tris.size(); i++) {
+		midpt = midpt + (tris[i]->getMidPoint() * (1.0 / tris.size()));
+	}
+
+	std::vector<OurTriangle*> left_tris;
+	std::vector<OurTriangle*> right_tris;
+	int axis = node->bbox.longestAxis();
+	for (int i = 0; i < tris.size(); i++) {
+		switch (axis) {
+		case X:
+			midpt[0] >= tris[i]->getMidPoint()[0] ? right_tris.push_back(tris[i]) : left_tris.push_back(tris[i]);
+			break;
+		case Y:
+			midpt[1] >= tris[i]->getMidPoint()[1] ? right_tris.push_back(tris[i]) : left_tris.push_back(tris[i]);
+			break;
+		case Z:
+			midpt[2] >= tris[i]->getMidPoint()[2] ? right_tris.push_back(tris[i]) : left_tris.push_back(tris[i]);
+			break;
+		}
+	}
+
+	if (left_tris.size() == 0 && right_tris.size() > 0) left_tris = right_tris;
+	if (right_tris.size() == 0 && left_tris.size() > 0) right_tris = left_tris;
+
+	int matches = 0;
+	for (int i = 0; i < left_tris.size(); i++) {
+		for (int j = 0; j < right_tris.size(); j++) {
+			if (left_tris[i] == right_tris[j])
+				matches++;
+		}
+	}
+
+	if ((float)matches / left_tris.size() < 0.5 && (float)matches / right_tris.size() < 0.5) {
+		node->left = build(left_tris);
+		node->right = build(right_tris);
+	} else {
+		node->left = new KDNode();
+		node->right = new KDNode();
+		node->left->triangles = std::vector<OurTriangle*>();
+		node->right->triangles = std::vector<OurTriangle*>();
+	}
+
+	return node;
+}
---
+++ src/kdtree/kdtree.h
@@ -0,0 +1,20 @@
+#ifndef KDTREE_H
+#define KDTREE_H
+#include "../shapes/shapes.h"
+#include "../Vec3D.h"
+
+class KDNode;
+class KDTree : public OurObject {
+	public:
+		KDTree(std::vector<OurTriangle*>& triangles);
+		virtual bool intersect(const Vec3Df&, const Vec3Df&, Vec3Df&, Vec3Df&);
+		virtual Vec3Df shade(const Vec3Df&, const Vec3Df&, const Vec3Df&, const Vec3Df&);
+		virtual bool hasMat() { return _intersected != nullptr; };
+		virtual Material& getMat() { return _intersected->_mat; };
+		static KDNode* build(std::vector<OurTriangle*>& triangles);
+	private:
+		KDNode* _root;
+		OurTriangle* _intersected;
+};
+
+#endif
